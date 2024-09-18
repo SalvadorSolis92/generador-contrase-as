c3b2e13ec94f21a679b1b8c277a24fe89b4cd02c
Salvador Solis <salvador920110@gmail.com>
2024-09-18 10:12:25 -0600

    implementación de la función para generar contraseña

--- proyecto/main.js
+++ proyecto/main.js
@@ -1,7 +1,29 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena');
 
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
+function generar(){
 
+    let numeroDigitado = parseInt(cantidad.value);
+    let password = '';  
 
+    if (cantidad.value < 8) {
+        alert('La longitud de la contraseña debe ser mayor a 8 caracteres');
+    }else{
+
+        for(let i = 0; i < numeroDigitado; i++){
+            let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+            password += caracterAleatorio;
+        }
+
+        console.log('pasword generado ' + password);
+        contrasena.value = password;
+    } 
+
+    
+}
 
 
 
